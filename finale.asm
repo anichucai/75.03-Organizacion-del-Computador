0d67202d70a12ffc7e6e2aa310d568d83b69adfd
anichucai <43823759+anichucai@users.noreply.github.com>
2019-06-10 17:39:57 -0300

    Update finale.asm

--- finale.asm
+++ finale.asm
@@ -29,6 +29,7 @@ section		.data
 
 	lenRegister		dd	32
 
+	msgLuego 		db "Luego de iterar...",10,0
 
 ;*************************************************************************
 ;							SECTION .BSS
@@ -40,6 +41,10 @@ section		.bss
 
 	posVector 				resd	1
 	vector		times	30	resd	1
+
+	num1	 				resd	1
+	num2	 				resd	1
+	aux	 					resd	1
 
 
 ;*************************************************************************
@@ -56,7 +61,17 @@ main:
 	call 	readBinaryFile
 
 	mov 	dword[posVector], 1
+	call 	printVector 
+
+	mov 	dword[posVector], 2
 	call 	iterateVector
+
+	push	msgLuego
+	call 	printf
+	add 	esp, 4
+
+	mov 	dword[posVector], 1
+	call 	printVector 
 
 	ret
 
@@ -128,6 +143,79 @@ endReadBinaryFile:
 ;_______________________________insertion___________________________________
 iterateVector:
 
+; busco num2
+
+	mov		eax,dword[posVector]
+	dec		eax		
+	imul	dword[lenRegister]		
+	lea		eax,[vector+eax]
+
+	mov 	eax, dword[eax]
+	mov 	dword[num2], eax
+
+; busco num1
+
+	mov		eax,dword[posVector]
+	dec		eax
+	dec		eax				
+	imul	dword[lenRegister]		
+	lea		eax,[vector+eax]
+
+	mov 	eax, dword[eax]
+	mov 	dword[num1], eax
+
+;resto num1-num2
+	mov 	eax, [num1]
+	mov 	ebx, [num2]
+	sub 	eax, ebx
+
+; num1<num2?
+	cmp 	eax, 0
+	jle 	continue
+
+; si num1 mayor, imprimir
+	call  	swap
+
+
+continue:
+
+	inc		dword[posVector]
+	mov     eax, dword[lenVector]
+	cmp		dword[posVector],eax
+	jle		iterateVector			
+
+	ret
+
+swap:
+
+; guardo en aux
+	mov eax, dword[num2]
+	mov dword[aux], eax
+
+; busco num2
+	mov		eax,dword[posVector]
+	dec		eax		
+	imul	dword[lenRegister]		
+	lea		eax,[vector+eax]
+
+	mov 	edx, dword[num1]
+	mov 	dword[eax], edx
+
+; busco num1
+
+	mov		eax,dword[posVector]
+	dec		eax
+	dec		eax				
+	imul	dword[lenRegister]		
+	lea		eax,[vector+eax]
+
+	mov 	edx, dword[aux]
+	mov 	dword[eax], edx
+
+	ret
+
+printVector:
+
 	mov		eax,dword[posVector]	;eax = posicion
 	dec		eax						;eax = posicion - 1
 	imul	dword[lenRegister]		;eax = (posicion - 1) * 10
@@ -141,7 +229,8 @@ iterateVector:
 	inc		dword[posVector]
 	mov     eax, dword[lenVector]
 	cmp		dword[posVector],eax
-	jle		iterateVector			
+	jle		printVector			
 
 	ret
+
 
